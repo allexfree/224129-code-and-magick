dbca950c285d3df7a54844d7116981d0a4c31643
allefree <allexfree@yandex.ru>
2018-06-11 19:02:30 +0300

    Доработка кода

--- js/setup.js
+++ js/setup.js
@@ -2,66 +2,60 @@
 
 // Объявление переменных
 
-var NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green', ''];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARDS = [];
+var names = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green', ''];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var wizards = [];
+
+for (var i = 0; i < 6; i++) {
+  wizards.push({name: names[i], surname: surnames[i], eyesColor: eyesColor[i], coatColor: coatColor[i]});
+} // формирование масиива WIZARDS
+
+
+// Вершины
 var fragment = document.createDocumentFragment();
 var similarWizardsList = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-for (var i = 0; i < 6; i++) {
-  var descriptionWizard = {};
-  var names = NAMES[i];
-  var surnames = SURNAMES[i];
-  var eyesColor = EYES_COLOR[i];
-  var coatColor = COAT_COLOR[i];
-  descriptionWizard.names = names;
-  descriptionWizard.surname = surnames;
-  descriptionWizard.eyesColor = eyesColor;
-  descriptionWizard.coatColor = coatColor;
-  WIZARDS.push(descriptionWizard);
-} // формирование масиива WIZARDS
-
-// Вершины
-
 
 // Определение ф-ций
 
-/* Ф-ция removeElements показывает элементы блоков, у котороых проставлен class="hidden и атрибут style="display: none" */
-var removeElements = function () {
-  var elementHiddenClass = document.querySelectorAll('.hidden');
+/* Ф-ция showElements показывает элементы блоков, у котороых проставлен class="hidden и атрибут style="display: none" */
+var showElements = function () {
+  var hiddenElements = document.querySelectorAll('.setup, .setup-similar');
+  for (i = 0; i < hiddenElements.length; i++) {
+    hiddenElements[i].classList.remove('hidden');
+  }
   var elementHiddenAttribute = document.querySelector('#similar-wizard-template');
-  elementHiddenClass[0].classList.remove('hidden');
-  elementHiddenClass[1].classList.remove('hidden');
   elementHiddenAttribute.removeAttribute('style', 'display: none');
 };
 
-/* Ф-ция getRandom возвращает случайное число от 0 до длины массива WIZARDS */
-var getRandom = function () {
-  return Math.floor(Math.random() * WIZARDS.length);
+/* Ф-ция getRandomArrayElement получает случайный элемент массива указанного в парметре array*/
+var getRandomArrayElement = function (array) {
+  return Math.floor(Math.random() * array);
 };
 
-/* Ф-ция fillElements выполняет заполнение блока элементами на основе массива WIZARDS */
-var fillElements = function () {
-  wizardElement.querySelector('.setup-similar-label').textContent = (WIZARDS[getRandom()].names + WIZARDS[getRandom()].surname);
-  wizardElement.querySelector('.wizard-coat').style.fill = WIZARDS[getRandom()].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = WIZARDS[getRandom()].eyesColor;
+/* Ф-ция fillElements выполняет заполнение блока элементами на основе массива wizards */
+var fillElements = function (element) {
+  element.querySelector('.setup-similar-label').textContent = (wizards[getRandomArrayElement(wizards.length)].name + wizards[getRandomArrayElement(wizards.length)].surname);
+  element.querySelector('.wizard-coat').style.fill = wizards[getRandomArrayElement(wizards.length)].coatColor;
+  element.querySelector('.wizard-eyes').style.fill = wizards[getRandomArrayElement(wizards.length)].eyesColor;
 };
 
-/* Ф-ция addElements добавдяет заполненые DOM-элементы в блок .setup-similar-list*/
-var addElements = function () {
-  fragment.appendChild(wizardElement);
+/* Ф-ция addElements добавляет заполненые DOM-элементы в блок .setup-similar-list*/
+var addElements = function (element) {
+  fragment.appendChild(element);
   similarWizardsList.appendChild(fragment);
 };
 
+
 // Вызов ф-ций
 
-removeElements();
+showElements();
 
 for (i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  fillElements();
-  addElements();
+  fillElements(wizardElement);
+  addElements(wizardElement);
 }
