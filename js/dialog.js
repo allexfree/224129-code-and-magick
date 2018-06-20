174cb3dd9c2ba27e6900f9f1a640c06e86a56bec
allefree <allexfree@yandex.ru>
2018-06-20 13:56:52 +0300

    Реализация сброса положения диалога на изначальное; формирование правильного модуля dialog; удаление ненужного кода из setup; при нажатии на клавишу ESC диалог не закрывается, если курсор стоит в поле .setup-user-name

--- js/dialog.js
+++ js/dialog.js
@@ -4,13 +4,25 @@
 
   // Объявление переменных
 
+  var blockSetup = document.querySelector('.setup');
+  var blockSetupClose = blockSetup.querySelector('.setup-close');
+  var blockSetupOpenIcon = document.querySelector('.setup-open-icon');
+  var buttonSubmit = document.querySelector('.setup-submit');
+  var setupWizard = document.querySelector('.setup-wizard');
+  var wizardCoat = setupWizard.querySelector('.wizard-coat');
+  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+  var fireball = document.querySelector('.setup-fireball-wrap');
+  var inputCoatColor = document.querySelector('input[name="coat-color"]');
+  var inputEyesColor = document.querySelector('input[name="eyes-color"]');
+  var inputFireballColor = document.querySelector('input[name="fireball-color"]');
+
   var startCoords;
   var dragged;
   var shift;
 
-  var blockSetupDialog = window.blockSetup;
-  var blockUploadDialog = blockSetupDialog.querySelector('.upload');
-  var artifactCell = blockSetupDialog.querySelector('.setup-artifacts-cell');
+  var blockUploadDialog = blockSetup.querySelector('.upload');
+  var setupInputName = blockSetup.querySelector('.setup-user-name');
+  var artifactCell = blockSetup.querySelector('.setup-artifacts-cell');
   var artifactCellImg = artifactCell.querySelector('img');
 
 
@@ -19,9 +31,42 @@
   artifactCell.setAttribute('style', 'position: relative');
   artifactCellImg.setAttribute('style', 'position: absolute; z-index: 1000');
 
+
   // Определение ф-ций
 
-  /*Ф-ция blockUploadMousedownHandler */
+  /* Ф-ция closeDialogWindow добавляет класс hidden блоку .setup и устанавливает атрибуты style top/left блокам artifactCellImg и .setup*/
+  var closeDialogWindow = function () {
+    blockSetup.classList.add('hidden');
+    artifactCellImg.style.top = '0';
+    artifactCellImg.style.left = '0';
+    blockSetup.style.top = '80px';
+    blockSetup.style.left = '50%';
+  };
+
+  /* Ф-ция openDialogWindow удаляет класс hidden у блока .setup */
+  var openDialogWindow = function () {
+    blockSetup.classList.remove('hidden');
+  };
+
+  /* Ф-ция setSubmitAttribute добавляет атрибут type="submit" кнопке button */
+  var setSubmitAttribute = function () {
+    buttonSubmit.setAttribute('type', 'submit');
+  };
+
+  /* Ф-ция changeCoatsAndEyesColor устанавливает атрибут style="fill:..." блоку указанному в парметре elementOne и атрибут value блоку, указанному в параметре elementTwo, цвет берется из параметра array */
+  var changeCoatsAndEyesColor = function (array, elementOne, elementTwo) {
+    var color = getRandomArrayElement(array); // в переменную передается рандомный элемент массива array
+    elementOne.setAttribute('style', 'fill:' + color);
+    elementTwo.setAttribute('value', color);
+  };
+
+  /* Ф-ция changeFireballColor устанавливает атрибут style="background-color:..." блоку указанному в парметре elementOne и атрибут value блоку, указанному в параметре elementTwo, цвет берется из параметра array */
+  var changeFireballColor = function (array, elementOne, elementTwo) {
+    var color = '#' + getRandomArrayElement(array);
+    elementOne.setAttribute('style', 'background-color:' + color);
+    elementTwo.setAttribute('value', color);
+  };
+
   var blockUploadMousedownHandler = function (evt) {
     evt.preventDefault();
     startCoords = {x: evt.clientX, y: evt.clientY};
@@ -34,8 +79,8 @@
       shift = {x: startCoords.x - moveEvt.clientX, y: startCoords.y - moveEvt.clientY};
       startCoords = {x: moveEvt.clientX, y: moveEvt.clientY};
 
-      blockSetupDialog.style.top = (blockSetupDialog.offsetTop - shift.y) + 'px';
-      blockSetupDialog.style.left = (blockSetupDialog.offsetLeft - shift.x) + 'px';
+      blockSetup.style.top = (blockSetup.offsetTop - shift.y) + 'px';
+      blockSetup.style.left = (blockSetup.offsetLeft - shift.x) + 'px';
     };
 
     var mouseUpHandler = function (upEvt) {
@@ -85,9 +130,55 @@
 
   // Обработчики событий
 
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.ESC_KEYCODE && evt.target !== setupInputName) {
+      closeDialogWindow();
+    }
+  });
+
+  blockSetupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.ENTER_KEYCODE) {
+      closeDialogWindow();
+    }
+  });
+
+  blockSetupOpenIcon.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openDialogWindow();
+    }
+  });
+
+  buttonSubmit.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      setSubmitAttribute();
+    }
+  });
+
+  blockSetupClose.addEventListener('click', function () {
+    closeDialogWindow();
+  });
+
+  blockSetupOpenIcon.addEventListener('click', function () {
+    openDialogWindow();
+  });
+
+  buttonSubmit.addEventListener('click', function () {
+    setSubmitAttribute();
+  });
+
+  wizardCoat.addEventListener('click', function () {
+    changeCoatsAndEyesColor(coatColor, wizardCoat, inputCoatColor);
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    changeCoatsAndEyesColor(eyesColor, wizardEyes, inputEyesColor);
+  });
+
+  fireball.addEventListener('click', function () {
+    changeFireballColor(fireballColor, fireball, inputFireballColor);
+  });
+
   blockUploadDialog.addEventListener('mousedown', blockUploadMousedownHandler);
   artifactCellImg.addEventListener('mousedown', starImgMousedownHandler);
-
-  // Вызов ф-ций
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -12,21 +12,12 @@ var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)'
 var fireballColor = ['ee4830', '30a8ee', '5ce6c0', 'e848d5', 'e6e848'];
 var wizards = [];
 
+
 // Вершины
+
 var fragment = document.createDocumentFragment();
 var similarWizardsList = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var blockSetup = document.querySelector('.setup');
-var blockSetupClose = blockSetup.querySelector('.setup-close');
-var blockSetupOpenIcon = document.querySelector('.setup-open-icon');
-var buttonSubmit = document.querySelector('.setup-submit');
-var setupWizard = document.querySelector('.setup-wizard');
-var wizardCoat = setupWizard.querySelector('.wizard-coat');
-var wizardEyes = setupWizard.querySelector('.wizard-eyes');
-var fireball = document.querySelector('.setup-fireball-wrap');
-var inputCoatColor = document.querySelector('input[name="coat-color"]');
-var inputEyesColor = document.querySelector('input[name="eyes-color"]');
-var inputFireballColor = document.querySelector('input[name="fireball-color"]');
 
 
 // Определение ф-ций
@@ -58,84 +49,6 @@ var addElements = function (element) {
   fragment.appendChild(element);
 };
 
-/* Ф-ция closeDialogWindow добавляет класс hidden блоку .setup */
-var closeDialogWindow = function () {
-  blockSetup.classList.add('hidden');
-};
-
-/* Ф-ция closeDialogWindow удаляет класс hidden у блока .setup */
-var openDialogWindow = function () {
-  blockSetup.classList.remove('hidden');
-};
-
-/* Ф-ция setSubmitAttribute добавляет атрибут type="submit" кнопке button */
-var setSubmitAttribute = function () {
-  buttonSubmit.setAttribute('type', 'submit');
-};
-
-/* Ф-ция changeCoatsAndEyesColor устанавливает атрибут style="fill:..." блоку указанному в парметре elementOne и атрибут value блоку, указанному в параметре elementTwo, цвет берется из параметра array */
-var changeCoatsAndEyesColor = function (array, elementOne, elementTwo) {
-  var color = getRandomArrayElement(array); // в переменную передается рандомный элемент массива array
-  elementOne.setAttribute('style', 'fill:' + color);
-  elementTwo.setAttribute('value', color);
-};
-
-/* Ф-ция changeFireballColor устанавливает атрибут style="background-color:..." блоку указанному в парметре elementOne и атрибут value блоку, указанному в параметре elementTwo, цвет берется из параметра array */
-var changeFireballColor = function (array, elementOne, elementTwo) {
-  var color = '#' + getRandomArrayElement(array);
-  elementOne.setAttribute('style', 'background-color:' + color);
-  elementTwo.setAttribute('value', color);
-};
-
-// Обработчики событий
-
-document.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closeDialogWindow();
-  }
-});
-
-blockSetupClose.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    closeDialogWindow();
-  }
-});
-
-blockSetupOpenIcon.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    openDialogWindow();
-  }
-});
-
-buttonSubmit.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    setSubmitAttribute();
-  }
-});
-
-blockSetupClose.addEventListener('click', function () {
-  closeDialogWindow();
-});
-
-blockSetupOpenIcon.addEventListener('click', function () {
-  openDialogWindow();
-});
-
-buttonSubmit.addEventListener('click', function () {
-  setSubmitAttribute();
-});
-
-wizardCoat.addEventListener('click', function () {
-  changeCoatsAndEyesColor(coatColor, wizardCoat, inputCoatColor);
-});
-
-wizardEyes.addEventListener('click', function () {
-  changeCoatsAndEyesColor(eyesColor, wizardEyes, inputEyesColor);
-});
-
-fireball.addEventListener('click', function () {
-  changeFireballColor(fireballColor, fireball, inputFireballColor);
-});
 
 // Вызов ф-ций
 
